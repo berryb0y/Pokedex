143b6c32b8d0fee16e367fb332d25da9b587ed3b
berryb0y <ab4537@gmail.com>
2022-02-28 21:28:10 -0600

    pokedex addition
    added 3 pokemon in an array to my script

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
-alert('Hello World');
-let favoriteFood = 'bbq ribs';
-document.write(favoriteFood);
+let pokemonList=[
+    {name: "bulbasaur", 
+    height: "28",
+    weight: "15.2", 
+    type: ["grass", "poison"]},
+    {name: "charmander",
+    height: "24",
+    weight: "18.7",
+    type: "fire"},
+    {name: "squirtle",
+    height: "20",
+    weight: "19.8",
+    type: "water"}];
