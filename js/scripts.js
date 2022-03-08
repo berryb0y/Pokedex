8cb89f553effcfeef346ed55adc209dd057122a3
berryb0y <ab4537@gmail.com>
2022-03-08 11:32:27 -0600

    new functions and forEach

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,7 @@
 // height - meters
 // weight - pounds
 
-// Below the pokemonList array in your “scripts.js” file, create a for loop that iterates over each item in pokemonList:
-// Use document.write() inside the loop’s code to write the Pokémon name on your website’s DOM.
-// Use what you’ve learned about adding strings in JavaScript to write the Pokémon’s height next to its name, for example, “Bulbasaur (height: 7)”.
+let pokemonRepository = (function (){
 let pokemonList = [
     {name: "Bulbasaur",
     number: "001",
@@ -27,23 +25,65 @@ let pokemonList = [
     number: "007",
     height: "0.6",
     weight: "19.8",
-    type: "water"}
-];
-let emptyObject = {};
-for (let i = 0; i < pokemonList.length; i++){
-    if (pokemonList[i].height > 1.5){
-        document.write(pokemonList[i].name + " " + pokemonList[i].height + " m " + " This is a big pokemon!! <br/>")
-    }else if (pokemonList[i].height > 0.5 && pokemonList[i].height < 1.5){
-        document.write(pokemonList[i].name + " " + pokemonList[i].height + "m <br/>")
-    }else {document.write(pokemonList[i].name + " " + pokemonList[i].height + "m <br/>")};
-}
+    type: "water"}];
 
-// for (let i = 0; i < pokemonList.length; i++){
-    // if (pokemonList[i].height > 1.5){
-        // document.write(pokemonList[i].name + " This is a big pokemon!! ")
-    // }else if (pokemonList[i].height > 0.5 && pokemonList[i].height < 1.5){
-        // document.write(pokemonList[i].name + " This is an average pokemon ")
-    // }else {document.write(pokemonList[i].name + " This is a small pokemon ")};
-// }
 
-    
+    function add(pokemon){
+        pokemonList.push(pokemon);
+    }
+
+    function getAll(pokemonList){
+        return pokemonList;
+    }
+
+    return{
+        add: add,
+        getAll: getAll
+    };
+})();
+
+
+function pokemons(pokemon){
+    if (list[i].height > 1.5){
+        document.write(list[i].name + " " + list[i].height + "m " + " This is a big pokemon!! <br/>")
+        }else if (list[i].height > 0.5 && list[i].height < 1.5){
+        document.write(list[i].name + " " + list[i].height + "m <br/>")
+         }else {document.write(list[i].name + " " + list[i].height + "m <br/>")};
+};
+
+pokemonRepository.getAll().forEach(pokemons);
+
+
+
+
+
+
+
+
+
+
+
+// let pokemonList1 = {
+//     name: "Bulbasaur",
+//     number: "001",
+//     height: "0.7",
+//     weight: "15.2", 
+//     type: ["grass", "poison"]
+// };
+
+// Object.keys(pokemonList1.forEach(function(property) {
+//     document.write(pokemonList1[property]);
+// });
+
+// function printArrayDetails(list){
+//     for (let i = 0; i < list.length; i++){
+//         if (list[i].height > 1.5){
+//         document.write(list[i].name + " " + list[i].height + "m " + " This is a big pokemon!! <br/>")
+//         }else if (list[i].height > 0.5 && list[i].height < 1.5){
+//         document.write(list[i].name + " " + list[i].height + "m <br/>")
+//          }else {document.write(list[i].name + " " + list[i].height + "m <br/>")};
+//     }
+// }   
+// printArrayDetails(pokemonList);
+
+
